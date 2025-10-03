978f96c818a848a1fe6a2021ad8984766dfc2e0d
Navaneeth Krishna <admin@webfork.in>
2025-10-03 20:24:14 +0530

    Update heartbeat request to include secret key and increment version number to 6

--- main.py
+++ main.py
@@ -11,11 +11,23 @@ ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
 
 URL = "https://webhook.site/984075aa-5d14-4e1c-a40a-420d379d6b5c"
+SECRET_KEY="OvFmiQxVrvMj0oydYFnapKfmmpi0tlNs"
+
+# def ensure_wifi_connection():
+#     """Checks Wi-Fi status and reconnects if necessary."""
+#     if not wlan.isconnected():
+#         print("Wi-Fi connection lost. Reconnecting...")
+#         wlan.disconnect()
+#         wlan.connect(SSID, PASSWORD)
+#         while not wlan.isconnected():
+#             print(".")
+#             time.sleep(1)
+#         print("Wi-Fi reconnected! IP:", wlan.ifconfig()[0])
 
 while True:
     try:
         print("Sending heartbeat...")
-        response = urequests.get(URL, timeout=5)
+        response = urequests.get(f"{URL}?key={SECRET_KEY}", timeout=5)
         response.close()
         print("Heartbeat sent successfully.")
     except Exception as e:
