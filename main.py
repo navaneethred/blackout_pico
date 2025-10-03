9a051b005e2c66eee6d6188a21d8f139da50a8bb
Navaneeth Krishna <admin@webfork.in>
2025-10-03 12:01:07 +0530

    Add LED blinking functionality to main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ firmware_url = "https://raw.githubusercontent.com/navaneethred/blackout_pico/"
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
 ota_updater.download_and_install_update_if_available()
+
+from machine import Pin
+import time
+
+led = Pin("LED", Pin.OUT)
+
+while True:
+    led.value(1)
+    time.sleep(1)
+    led.value(0)
+    time.sleep(1)
