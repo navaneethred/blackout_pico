a4e5a02d7047992d6942cda97fa33e618c8797b3
Navaneeth Krishna <admin@webfork.in>
2025-10-03 20:38:01 +0530

    Refactor Wi-Fi connection handling and update heartbeat URL; increment version to 7

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 import network
 import time
 import urequests
 
-firmware_url = "https://raw.githubusercontent.com/navaneethred/blackout_pico/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-
-ota_updater.download_and_install_update_if_available()
-
-URL = "https://webhook.site/984075aa-5d14-4e1c-a40a-420d379d6b5c"
+WIFI_SSID = SSID
+WIFI_PASSWORD = PASSWORD
+URL = "https://blk.wfrk.net/heartbeat"
 SECRET_KEY="OvFmiQxVrvMj0oydYFnapKfmmpi0tlNs"
 
-# def ensure_wifi_connection():
-#     """Checks Wi-Fi status and reconnects if necessary."""
-#     if not wlan.isconnected():
-#         print("Wi-Fi connection lost. Reconnecting...")
-#         wlan.disconnect()
-#         wlan.connect(SSID, PASSWORD)
-#         while not wlan.isconnected():
-#             print(".")
-#             time.sleep(1)
-#         print("Wi-Fi reconnected! IP:", wlan.ifconfig()[0])
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+def ensure_wifi_connection():
+    if not wlan.isconnected():
+        print("Wi-Fi connection lost. Reconnecting...")
+        wlan.disconnect()
+        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
+        while not wlan.isconnected():
+            print(".")
+            time.sleep(1)
+        print("Wi-Fi reconnected! IP:", wlan.ifconfig()[0])
+
+ensure_wifi_connection()
 
 while True:
+    ensure_wifi_connection()
+
     try:
         print("Sending heartbeat...")
-        response = urequests.get(f"{URL}?key={SECRET_KEY}", timeout=5)
+        response = urequests.get(URL, params={"key": SECRET_KEY}, timeout=5)
         response.close()
         print("Heartbeat sent successfully.")
     except Exception as e:
